c478cafff83b2c08a2623582ceac8a01e805f080
Bunyodbek Shamsiddinov <bshamsid@u80z03s07.42kl.edu.my>
2022-12-19 18:21:03 +0800

    finished 02

--- ex02/Array.hpp
+++ ex02/Array.hpp
@@ -11,38 +11,74 @@ class Array
 		// Constructors
 		Array()
 		{
-			*_arr = 0;
+			_arr = new T[0];
 			_len = 0;
 			std::cout << "\e[0;33mDefault Constructor called of Array\e[0m" << std::endl;
 		}
+
 		Array(unsigned int n)
 		{
 			_arr = new T[n];
 			_len = n;
+			for (unsigned int i = 0; i < _len; i++) {
+  				_arr[i] = 0;
+  			}
 			std::cout << "\e[0;33mDefault Constructor called of Array\e[0m" << std::endl;
 		}
+
 		Array(const Array &copy)
 		{
-			(void) copy;
+			_arr = new T[copy._len];
+			if (copy._len == 0) {
+				_len = 0;
+			}
+			else {
+				for (unsigned int i = 0; i < copy._len; i++) {
+  					_arr[i] = copy._arr[i];
+  				}
+				_len = copy._len;
+			}
 			std::cout << "\e[0;33mCopy Constructor called of Array\e[0m" << std::endl;
 		}
 		
 		// Destructor
 		~Array()
 		{
+			delete _arr;
 			std::cout << "\e[0;31mDestructor called of Array\e[0m" << std::endl;
 		}
 
 		
 		// Operators
+		Array &operator=(const Array &copy)
+		{
+			if (copy._len == 0) {
+				delete _arr;
+				_arr = new T[0];
+				_len = 0;
+			}
+			else {
+				delete _arr;
+				_arr = new T[copy._len];
+				for (unsigned int i = 0; i < copy._len; i++) {
+  					_arr[i] = copy._arr[i];
+  				}
+				_len = copy._len;
+			}
+			return *this;
+		}
+
 		T &operator[](unsigned int index)
 		{
 			if (index >= _len)
 				throw  Array::IndexOutOfRangeException();
-			return _arr[index];
+			return (_arr[index]);
 		}
 		
-		unsigned int size( void ) const;
+		unsigned int size( void ) const
+		{
+			return _len;
+		}
 
 		class IndexOutOfRangeException : public std::exception
 		{
